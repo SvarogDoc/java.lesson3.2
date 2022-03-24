ea561542204f3fe714f348bc2dae3050e8fd7046
SvarogDoc <learnqai33@gmail.com>
2022-03-25 00:53:22 +0300

    code correction

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,5 @@
 public class BmiService {
-    public float calculate(int index) {
-        int weight = 90;
-        int height = 182;
+    public float calculate(int weight, int height) {
         float bodyMassIndex = (float) weight / (height * height) * 100 * 100;
         return bodyMassIndex;
 
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService service = new BmiService();
         int weight = 90;
         int height = 182;
-        float bodyMassIndex = service.calculate(weight);
+        float bodyMassIndex = service.calculate(weight, height);
         System.out.println("Ваш индекс массы тела составляет=" + bodyMassIndex + " кг/кв.м");
     }
 }
